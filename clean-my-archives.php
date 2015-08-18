d3794f82bb30e68d8b16e43a25f0ddf62281ef5b
Justin Tadlock <justin@justintadlock.com>
2015-08-17 19:44:05 -0500

    Version bump to 1.0.0-dev.

--- clean-my-archives.php
+++ clean-my-archives.php
@@ -3,22 +3,22 @@
  * Plugin Name: Clean My Archives
  * Plugin URI: http://themehybrid.com/plugins/clean-my-archives
  * Description: A plugin that displays a full archive of posts by month and year with the <code>[clean-my-archives]</code> shortcode.
- * Version: 0.2.0
+ * Version: 1.0.0-dev
  * Author: Justin Tadlock
  * Author URI: http://justintadlock.com
  *
- * Clean My Archives is a plugin developed to simplify the process of adding a list of archives to your 
- * site.  So many archives plugins make things overly complex or add a lot of junk to the page like unneeded 
+ * Clean My Archives is a plugin developed to simplify the process of adding a list of archives to your
+ * site.  So many archives plugins make things overly complex or add a lot of junk to the page like unneeded
  * JavaScript.  This plugin was created to clean your archives page.
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
- * General Public License as published by the Free Software Foundation; either version 2 of the License, 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
  * or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * You should have received a copy of the GNU General Public License along with this program; if not, write 
+ * You should have received a copy of the GNU General Public License along with this program; if not, write
  * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * @package   CleanMyArchives
